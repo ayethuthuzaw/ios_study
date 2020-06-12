186eb79cb8c39d65ef485661b2a13f8f5e8e66ca
ayethuthuzaw <ayethuthuzaw@al-j.co.jp>
2020-06-12 15:35:59 +0900

    add 005_Quiz

---
+++ Quiz/Quiz/QuizViewController.swift
@@ -0,0 +1,118 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by Aye Thu Thu Zaw on 2020/06/08.
+//  Copyright © 2020 ALJ. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    func loadProblemSet() {
+        
+       let path = Bundle.main.path(forResource: "quiz", ofType: "csv")
+    
+       let enc = String.Encoding.utf8
+       let data = NSData(contentsOfFile: path!)
+        let text = String(NSString(data: data! as Data, encoding: enc.rawValue)!)
+
+       let lines = text.components(separatedBy: "\n")
+
+       for i in 0..<lines.count {
+           let items = lines[i].components(separatedBy: ",")
+           let p = Problem()
+           let q = items[0]
+           let a = Int(items[1])
+           p.setQ(q: q, a: a!)
+           problemSet.add(p)
+       }
+    }
+    
+        func shuffleProblemSet() {
+
+            let totalQuestions = problemSet.count
+            var i = totalQuestions
+
+            while i > 0 {
+
+                let j = arc4random() % UInt32(i)
+                problemSet.exchangeObject(at: i-1, withObjectAt: Int(j))
+
+                i -= 1
+            }
+        }
+    
+        
+    @IBOutlet weak var problemText: UITextView!
+    
+    var problemSet = NSMutableArray()
+    var totalProblems = Int()
+    var currentProblem = Int()
+    var correctAnswers = Int()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.loadProblemSet()
+        self.shuffleProblemSet()
+        
+        totalProblems = 10
+        currentProblem = 0
+        correctAnswers = 0
+        
+        let questions = problemSet.object(at: currentProblem) as! Problem
+        problemText.text = questions.getQ()
+    }
+    
+    func nextProblem() {
+
+        currentProblem += 1
+
+        if currentProblem < totalProblems {
+
+            let questions = problemSet.object(at: currentProblem) as! Problem
+            problemText.text = questions.getQ()
+        } else {
+            self.performSegue(withIdentifier: "toResultView", sender:self)
+        }
+    }
+    
+        override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+            let percentage = correctAnswers * 100 / totalProblems
+            
+
+            if segue.identifier == "toResultView" {
+                let rvc = segue.destination as! ResultViewController
+                rvc.correctPercentage = percentage
+            }
+        }
+    
+    @IBAction func answerIsTrue(_ sender: Any) {
+        let questions = problemSet.object(at: currentProblem) as! Problem
+           if questions.getA() == 0 {
+               correctAnswers += 1;
+           }
+           self.nextProblem()
+    }
+    
+    @IBAction func answerIsFalse(_ sender: Any) {
+        let questions = problemSet.object(at: currentProblem) as! Problem
+        if questions.getA() == 1 {
+            correctAnswers += 1;
+        }
+        self.nextProblem()
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
